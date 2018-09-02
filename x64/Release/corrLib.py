f2a5c838ebbb3bb060e23281cce24a4edac99324
Alexander Craddock <acraddoc@umd.edu>
2018-09-02 13:59:07 -0400

    Tidying up

--- x64/Release/corrLib.py
+++ x64/Release/corrLib.py
@@ -175,3 +175,27 @@ def g2ToFile_cpu(folder_name,file_out_name,max_time,bin_width,pulse_spacing,max_
     #    _ctypes.dlclose(lib._handle)
     tau = np.arange(-max_bin,max_bin+1) * int_bin_width
     scipy.io.savemat(file_out_name,{'numer':np.array(numer_list),'denom':denom_ctypes.value,'tau':tau})
+
+def g2ToFile_cpu_thread_benchmark(folder_name,file_out_name,max_time,bin_width,pulse_spacing,max_pulse_distance,cpu_threads):
+    file_list = os.listdir(folder_name)
+    int_bin_width = round(bin_width / tagger_resolution) * tagger_resolution
+    int_max_time = round(max_time / int_bin_width) * int_bin_width
+    int_pulse_spacing = round(pulse_spacing / int_bin_width) * int_bin_width
+    max_bin  = int(round(int_max_time/int_bin_width))
+    num_files = len(file_list)
+    numer_list = [int(0)]*(2*max_bin + 1)
+    denom_ctypes = ctypes.c_int(0)
+    ctypes_file_list = file_list_to_ctypes(file_list, folder_name)
+    lib = ctypes.CDLL(working_directory + '/' + lib_name)
+    lib.getG2Correlations_cpu_thread_benchmark.argtypes = [ctypes.c_char_p * num_files, ctypes.c_int, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_int, ctypes.py_object, ctypes.POINTER(ctypes.c_int), ctypes.c_int]
+    start_time = time.time()
+    #with wurlitzer.sys_pipes():
+    lib.getG2Correlations_cpu_thread_benchmark(ctypes_file_list, num_files, int_max_time, int_bin_width, int_pulse_spacing, max_pulse_distance, numer_list, ctypes.byref(denom_ctypes), cpu_threads)
+    print("Finished in " + str(time.time()-start_time) + "s")
+    
+    #if os.name == 'nt':
+    #    _ctypes.FreeLibrary(lib._handle)
+    #else:
+    #    _ctypes.dlclose(lib._handle)
+    tau = np.arange(-max_bin,max_bin+1) * int_bin_width
+    #scipy.io.savemat(file_out_name,{'numer':np.array(numer_list),'denom':denom_ctypes.value,'tau':tau})
